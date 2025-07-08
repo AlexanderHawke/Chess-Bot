e8aad91fefbaf1be2f20470ea860716462db77fc
Alex Hawke <90803950+AlexanderHawke@users.noreply.github.com>
2025-07-07 22:07:20 -0400

    Evaluate function works, may need to tweak scoreBoards for pieces. Added player vs computer logic instead of black vs white for easier conceptualization of the GUI later on.

--- ChessBot.py
+++ ChessBot.py
@@ -2,14 +2,30 @@ class ChessGame:
 
     def __init__(self):
         
+        # White pieces defined and their score values
+        self.whitePieces = {"wP": 1,
+                            "wN": 3,
+                            "wB": 3,
+                            "wR": 5,
+                            "wQ": 9,
+                            "wK": 1000}
+        
+        # Black pieces defined and their score values
+        self.blackPieces = {"bP": 1,
+                            "bN": 3,
+                            "bB": 3,
+                            "bR": 5,
+                            "bQ": 9,
+                            "bK": 1000}
+
         # The board in the starting position with the white pieces facing the player.
         self.whiteBoardStarting = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-            [" ", " ", " ", " ", " ", " ", " ", " "],
-            [" ", " ", " ", " ", " ", " ", " ", " "],
-            [" ", " ", " ", " ", " ", " ", " ", " "],
-            [" ", " ", " ", " ", " ", " ", " ", " "],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
             ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
@@ -18,33 +34,30 @@ class ChessGame:
         self.blackBoardStarting = [
             ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
-            [" ", " ", " ", " ", " ", " ", " ", " "],
-            [" ", " ", " ", " ", " ", " ", " ", " "],
-            [" ", " ", " ", " ", " ", " ", " ", " "],
-            [" ", " ", " ", " ", " ", " ", " ", " "],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+            ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"]
         ]
-        
-        self.player = input("What side would you like to play as? (white or black) ").strip().lower()
-        while self.player != "black" and self.player != "white":
-            self.player = input("Please input an appropriate answer, what side would you like to play as? (white or black) ")
 
-        self.board = []
+        # Ask the player what colour pieces they would like to play
+        self.player = input("What side would you like to play as? ('white' or 'black') ").strip().lower()
+        while self.player != "black" and self.player != "white":
+            self.player = input("Please input an appropriate answer, what side would you like to play as? ('white' or 'black') ")
         
+        # set the board so that the player's pieces are on the bottom always, and assign player/computer Pieces
         if self.player == "white":
             self.board = self.whiteBoardStarting
-        if self.player == "black":
+            self.playerPieces = self.whitePieces
+            self.computerPieces = self.blackPieces
+        else:
             self.board = self.blackBoardStarting
+            self.playerPieces = self.blackPieces
+            self.computerPieces = self.whitePieces
         
-        self.materialCost = {"wP": 1, "bP": 1, # Pawns are worth 1
-                             "wN": 3, "bN": 3, # Knights are worth 3
-                             "wB": 3, "bB": 3, # Bishops are worth 3
-                             "wR": 5, "bR": 5, # Rooks are worth 5
-                             "wQ": 9, "bQ": 9, # Queens are worth 9
-                             "wK": 1000, "bK": 1000} # Kings are worth 1000
-        
-        # Pawn position scores (player perspective)
+        # Pawn position scores (PLAYER perspective)
         # Higher values for advancing pawns, center files favored
         self.pawnScores = [
             [90, 90, 90, 90, 90, 90, 90, 90], # Rank 8 - PROMOTION! Highest value
@@ -122,12 +135,44 @@ class ChessGame:
             [ 20, 30, 10,  0,  0, 10, 30, 20]
         ]
 
-        
+        # relates a piece to their respective score table for easy lookup of piece value (piece value * positional scoring)
+        self.scoreTables = {
+            "P": self.pawnScores,
+            "N": self.knightScores,
+            "B": self.bishopScores,
+            "R": self.rookScores,
+            "Q": self.queenScores,
+            "K": self.kingScores
+        }
 
+    # Prints the game board as a string
     def __str__(self):
         return  '\n'.join(' '.join(str(x) for x in row) for row in self.board)
-    
-    
+
+    # Evaluates the game state of the board (player favoured is positive, computer favoured is negative)
+    def evaluate(self):
+        playerScore = 0
+        computerScore = 0
+        for i in range(8):
+            for j in range(8):
+                if self.board[i][j] in self.playerPieces:
+                    playerScore += self.playerPieces[self.board[i][j]] * (1/self.scoreTables[self.board[i][j][1]][i][j])
+                if self.board[i][j] in self.computerPieces:
+                    computerScore += self.computerPieces[self.board[i][j]] * (1/self.scoreTables[self.board[i][j][1]][7-i][7-j])
+        
+        return playerScore - computerScore
 
 chessGame = ChessGame()
+chessGame.board = [
+                    ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+                    ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+                    ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+                    ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+                    ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+                    ["  ", "  ", "  ", "  ", "wP", "  ", "  ", "  "],
+                    ["wP", "wP", "wP", "wP", "  ", "wP", "wP", "wP"],
+                    ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+                ]
+
 print(chessGame)
+print("Evaluation: ", chessGame.evaluate())
